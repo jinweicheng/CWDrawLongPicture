b225d216efabd34e3cc539858f7453762f380d2d
程金伟 <chengjinweiiosdn88@163.com>
2018-10-28 19:30:32 +0800

    Initial Commit

---
+++ CWDrawLongPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CWDrawLongPicture
+//
+//  Created by 程金伟 on 2018/10/28.
+//  Copyright © 2018年 cjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CWDrawLongPicture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CWDrawLongPicture
+//
+//  Created by 程金伟 on 2018/10/28.
+//  Copyright © 2018年 cjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
